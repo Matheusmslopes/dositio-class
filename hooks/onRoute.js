575218d9c0167d6fdd60cf4a3139d74a61b3a601
alunodev02 <alunodev02@lab.uvv>
2024-03-11 08:59:18 -0300

    Initial commit

--- hooks/onRoute.js
+++ hooks/onRoute.js
@@ -1,17 +1,38 @@
 /** @type{import('fastify').FastifyPluginAsync<>} */
+import createError from '@fastify/error';
 export default async function onRouteHook(app, options) {
+    const AuthError = createError('AuthError', 'x-access-token is missing', 401);
+    const TokenError = createError('TokenError', 'Invalid Token', 401);
+    
     const logMe = async (request, reply) => {
         request.log.info(`Request for url: ${request.url}.`);
     };
 
+    const extractUser = async(req, rep) => {
+        if (!req.headers['x-access-token']){
+            throw new AuthError();
+        } else{
+            app.jwt.verify(req.headers['x-access-token'], (err, decoded) =>{
+                if (err) throw new TokenError();
+                else{
+                   req.user = deoced.username;
+                }
+            });
+        }
+    };
+
     app.addHook('onRoute', (routeOptions) => {
-        if(routeOptions.config?.logMe){
-            if(routeOptions.onRequest && !Array.isArray(routeOptions.onRequest)){
-                routeOptions.onRequest = [routeOptions.onRequest];
-            }else{
-                routeOptions.onRequest = [];
-            }
+        if(routeOptions.onRequest && !Array.isArray(routeOptions.onRequest)){
+            routeOptions.onRequest = [routeOptions.onRequest];
+        }else{
+            routeOptions.onRequest = [];
+        }
+
+        if (routeOptions.config?.logMe){
             routeOptions.onRequest.push(logMe);
+        }
+        if (routeOptions.config?.requireAuthentication){
+            routeOptions.onRequest.push(extractUser);
         }
     });
 }
