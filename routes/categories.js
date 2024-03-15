f45a99bb73f80aa3f3565acaccab4676b50a5fc7
alunodev02 <alunodev02@lab.uvv>
2024-03-15 08:48:58 -0300

    Add categories.js

---
+++ routes/categories.js
@@ -0,0 +1,18 @@
+/** @type{import('fastify').FastifyPluginAsync<>} */
+import createError from '@fastify/error';
+export default async function products(app, options) {
+    const InvalidProductError = createError('InvalidProductError', 'Produto Inválido.', 400);
+
+    const categories = app.mongo.db.collection('categorias');
+
+    app.get('/categories', 
+        {
+            config: {
+                logMe: true
+            }
+        }, 
+        async (req, rep) => {
+        return await categories.find().toArray();
+    });
+
+}
